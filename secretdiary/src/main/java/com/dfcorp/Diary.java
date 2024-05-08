6e237550ad248d210214117b5d8a3b8b8fa0a216
Kristy Lam <lamlokhinkristy@gmail.com>
2024-05-08 11:01:07 +0100

    Passed tests 1 to 7

--- secretdiary/src/main/java/com/dfcorp/Diary.java
+++ secretdiary/src/main/java/com/dfcorp/Diary.java
@@ -1,4 +1,31 @@
 package com.dfcorp;
 
+import java.util.ArrayList;
+
 public class Diary {
+    private ArrayList<Entry> entries;
+    private Boolean isLocked;
+    private final int defaultPin = 1234;
+
+    Diary() {
+        entries = new ArrayList<>();
+        isLocked = false;
+    }
+
+    public void unlock(int pin) {
+        if(pin == defaultPin) this.isLocked = false;
+    }
+
+    public void addEntry(Entry entry) {
+        if (!this.isLocked) { entries.add(entry); }
+    }
+
+    public ArrayList<Entry> getEntries() {
+        if(!this.isLocked) return entries;
+        else return null;
+    }
+
+    public Boolean isLocked() { return this.isLocked; }
+    public void isLocked(boolean isLocked) { this.isLocked = isLocked; }
+
 }
--- secretdiary/src/test/java/com/dfcorp/DiaryTest.java
+++ secretdiary/src/test/java/com/dfcorp/DiaryTest.java
@@ -22,15 +22,28 @@ public class DiaryTest {
     @DisplayName("Diary AddEntry Tests")
     class DiaryAddEntryTests {
 
-        // User story 1
+        // User Story 1
+        @Test
+        @DisplayName("Should add diary entry")
         public void diaryAddEntry() {
             // Arrange
             // Act
-            Entry testEntry = testDiary.addEntry(new Entry());
+            testDiary.addEntry(new Entry());
             // Assert
-            assertEquals(testDiary.getEntries.size(), 1);
+            assertEquals(testDiary.getEntries().size(), 1);
         }
 
+        // User Story 2
+        @Test
+        @DisplayName("Should not add diary entry when it's locked")
+        public void diaryNotToAddEntryIfLocked() {
+            // Arrange
+            // Act
+            testDiary.isLocked(true);
+            testDiary.addEntry(new Entry());
+            // Assert
+            assertEquals(testDiary.getEntries(), null);
+        }
 
         // User story 3
         @Test
@@ -45,6 +58,62 @@ public class DiaryTest {
 
             // Assert
             assertEquals(entries.get(0), testEntry);
+        }
+
+        // User story 4
+        @Test
+        @DisplayName("Should not display diary entries if locked")
+        public void getEntriesShouldNotReturnListOfEntriesIfDiaryLocked() {
+            // Arrange
+            Entry testEntry = new Entry();
+            testDiary.addEntry(testEntry);
+
+            // Act
+            testDiary.isLocked(true);
+            ArrayList<Entry> entries = testDiary.getEntries();
+
+            // Assert
+            assertNull(entries);
+        }
+    }
+
+    @Nested
+    @DisplayName("Diary Locking tests")
+    class DiaryLockingTests {
+        // User story 5
+        @Test
+        @DisplayName("Should be able to lock the diary")
+        public void shouldLockDiary() {
+            // Arrange
+            // Act
+            testDiary.isLocked(true);
+
+            // Assert
+            assertTrue(testDiary.isLocked());
+        }
+
+        // User Story 6
+        @Test
+        @DisplayName("Should not unlock diary if pin is incorrect")
+        public void diaryStayLockedWithIncorrectPin() {
+            // Arrange
+            Entry testEntry = new Entry();
+            testDiary.addEntry(testEntry);
+            testDiary.isLocked(true);
+            // Act
+            testDiary.unlock(2345);
+            // Assert
+            assertEquals(testDiary.getEntries(), null);
+        }
+
+        // User story 7
+        @Test
+        @DisplayName("Should unlock diary if pin is correct")
+        public void diaryShouldUnlockWithCorrectPin() {
+            testDiary.isLocked(true);
+            testDiary.unlock(1234);
+
+            assertFalse(testDiary.isLocked());
         }
     }
 }
